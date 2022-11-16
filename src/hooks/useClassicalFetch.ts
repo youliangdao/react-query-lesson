2c021031aee24244d223bd08859f5927f1c457d4
youliangdao <youliangdaojing5@gmail.com>
2022-11-16 09:22:50 +0900

    ✨feat: APIと連携するためのカスタムフックを実装

---
+++ src/hooks/useClassicalFetch.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useStateContext } from '../context/StateProvider'
+import { Task } from '../types/types'
+import { TASKS_ENDPOINT } from '../urls'
+
+export const useTasks = () => {
+  const { tasks, setTasks } = useStateContext()
+  const [isLoading, setLoading] = useState(false)
+  const [isError, setError] = useState(false)
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      setError(false)
+      setLoading(true)
+      try {
+        const result = await axios.get<Task[]>(TASKS_ENDPOINT)
+        setTasks(result.data)
+      } catch (error) {
+        setError(true)
+      }
+      setLoading(false)
+    }
+    fetchTasks()
+  }, [setTasks])
+
+  return {
+    tasks,
+    isLoading,
+    isError,
+  }
+}
